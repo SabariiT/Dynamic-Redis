8b281cc7758542526d301c81106432f76a257dfb
sabari.t <sabari.t@knowledgelens.com>
2023-09-06 22:47:30 +0530

    chore: Updated classifier

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['redis'],
     keywords=['python', 'redis', 'sentinel'],
     classifiers=[
-        "Development Status :: 1 - Testing",
+        "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
